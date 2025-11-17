7295bb1683dec268a85e76a1e675ce0b4677760a
NAVEEN KUMAR S <mailmeatnaveens@gmail.com>
2025-11-18 00:44:52 +0530

    Update bg_image.py

--- bg_image.py
+++ bg_image.py
@@ -1,19 +1,8 @@
-from tkinter import filedialog, messagebox
 from PIL import Image
-from bg_removal import remove_background
+from rembg import remove
 
-def add_background_image(img_path, bg_path=None):
-    if bg_path is None:
-        bg_path = filedialog.askopenfilename(title="Select Background Image",
-                                             filetypes=[("Image Files", "*.png;*.jpg;*.jpeg")])
-        if not bg_path:
-            return None
-    try:
-        fg = remove_background(img_path)
-        bg = Image.open(bg_path).convert("RGBA").resize(fg.size)
-        bg.paste(fg, (0, 0), fg)
-        messagebox.showinfo("Success", f"Background replaced with {bg_path}")
-        return bg
-    except Exception as e:
-        messagebox.showerror("Error", f"Could not add background image:\n{e}")
-        return None
+def add_image_background(img_path, bg_path):
+    fg = remove(Image.open(img_path)).convert("RGBA")
+    bg = Image.open(bg_path).convert("RGBA").resize(fg.size)
+    bg.paste(fg, (0, 0), fg)
+    return bg
