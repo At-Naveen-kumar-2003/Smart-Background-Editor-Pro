34197b1dd093cde6d894649f62e9fecc9a273c20
NAVEEN KUMAR S <mailmeatnaveens@gmail.com>
2025-11-18 00:44:31 +0530

    Update bg_color.py

--- bg_color.py
+++ bg_color.py
@@ -1,16 +1,8 @@
-from tkinter import colorchooser, messagebox
-from bg_removal import remove_background
 from PIL import Image
+from rembg import remove
 
-def add_background_color(img_path):
-    color = colorchooser.askcolor(title="Choose Background Color")
-    if not color or color[1] is None:
-        return None
-    chosen_hex = color[1]
-    result = remove_background(img_path)
-    if result:
-        fg = result.convert("RGBA")
-        bg = Image.new("RGBA", fg.size, chosen_hex)
-        bg.paste(fg, (0, 0), fg)
-        messagebox.showinfo("Success", "Color background added successfully.")
-        return bg
+def add_color_background(img_path, hex_color):
+    fg = remove(Image.open(img_path)).convert("RGBA")
+    bg = Image.new("RGBA", fg.size, hex_color)
+    bg.paste(fg, (0, 0), fg)
+    return bg
