280cd6a7f97767d65516655855a6823e1aea576b
NAVEEN KUMAR S <mailmeatnaveens@gmail.com>
2025-11-18 00:48:06 +0530

    Add image-based background replacement module

--- bg_image.py
+++ bg_image.py
@@ -1,6 +1,7 @@
 from PIL import Image
 from rembg import remove
 
+
 def add_image_background(img_path, bg_path):
     fg = remove(Image.open(img_path)).convert("RGBA")
     bg = Image.open(bg_path).convert("RGBA").resize(fg.size)
